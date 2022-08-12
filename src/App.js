44b28188851c4cfcf64352d779a49b1557557c47
adejoe22 <adedejiomogbehin@gmail.com>
2022-08-12 12:10:17 +0100

    new age

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import Navbar from "./pages/Navbar";
+import Hero from "./pages/Hero";
+import Platform from "./pages/Platform";
+import Testimonies from "./pages/Testimonies";
+import FAQ from "./FAQ";
+import Footer from "./Footer";
+
+function App() {
+  return (
+    <>
+      <Navbar />
+      <Hero />
+      <Platform />
+      <Testimonies />
+      <FAQ />
+      <Footer />
+    </>
+  );
+}
+
+export default App;
